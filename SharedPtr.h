94ccb962ecb7b3b9d372b73db136851ad5f1e1f0
Debarun De <debarun.de20@gmail.com>
2025-02-28 18:11:16 -0500

    jot some ideas down

--- SharedPtr.h
+++ SharedPtr.h
@@ -1,9 +1,18 @@
 #ifndef SHARED_PTR_H
 #define SHARED_PTR_H
 
+template <typename T>
 class SharedPtr {
     private:
+        T* ptr;
+        uint* count;
+        //mutex?
+        mutable std::mutex lock;
     public:
+        //default constructor
+        SharedPtr() : ptr(nullptr), count(new uint(0)) {
+            
+        }
 };
 
 #endif // SHARED_PTR_H
