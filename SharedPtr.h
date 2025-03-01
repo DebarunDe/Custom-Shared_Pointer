356f236df956ac03e66e6000e7844c9389b2c7f0
Debarun De <debarun.de20@gmail.com>
2025-03-01 17:18:51 -0500

    basic test functioning

--- SharedPtr.h
+++ SharedPtr.h
@@ -5,14 +5,96 @@ template <typename T>
 class SharedPtr {
     private:
         T* ptr;
-        uint* count;
+        unsigned int* count;
         //mutex?
-        mutable std::mutex lock;
     public:
         //default constructor
-        SharedPtr() : ptr(nullptr), count(new uint(0)) {
-            
+        SharedPtr() : ptr(nullptr), count(new unsigned int(0)) {}
+        //constructor with pointer
+        SharedPtr(T* ptr) : ptr(ptr), count(new unsigned int(1)) {}
+
+        //copy constructor
+        SharedPtr(const SharedPtr & obj) {
+            this->ptr = obj.ptr;
+            this->count = obj.count;
+            if (obj.ptr != nullptr) {
+                (*this->count)++;
+            }
         }
+        SharedPtr& operator=(const SharedPtr & obj) {
+            //If already pointing to same object, increment count and return
+            if (this != &obj) {
+                if (this->ptr != nullptr) {
+                    cleanup();
+                }
+                //if not pointing to a object, just copy pointer and count
+                this->ptr = obj.ptr;
+                this->count = obj.count;
+            }
+            //no matter what, increment the count
+            if (obj.ptr != nullptr) {
+                (*this->count)++;
+            }
+            return *this;
+        }
+
+        //move constructor
+        SharedPtr(SharedPtr && obj) {
+            this->ptr = obj.ptr;
+            this->count = obj.count;
+            obj.ptr = nullptr;
+            obj.count = nullptr;
+        }
+        SharedPtr& operator=(SharedPtr && obj) {
+            if (this != &obj) {
+                if (this->ptr != nullptr) {
+                    cleanup();
+                }
+                this->ptr = obj.ptr;
+                this->count = obj.count;
+            }
+            obj.ptr = nullptr;
+            obj.count = nullptr;
+            return *this;
+        }
+
+        //overloaded dereference operator
+        T* operator->() const {
+            return this->ptr;
+        }
+        T& operator*() const {
+            return *this->ptr;
+        }
+
+        //get count
+        unsigned int getCount() const {
+            return *this->count;
+        }
+        //get pointer
+        T* get() const {
+            return this->ptr;
+        }
+
+        //destructor
+        ~SharedPtr() {
+            cleanup();
+        }
+
+        private:
+            void cleanup() {
+                /* Decrement the count to current object assigned to current SharedPtr, and remove the ptr associated with it,
+                * if the count is 1, then we are the last ptr to object, so we can delete the object.
+                * if the count is 0, ptr is pointing to a nullptr, so we can just delete the count.
+                */
+               if (count && --(*this->count) == 0) {
+                    if (this->ptr != nullptr) {
+                        delete this->ptr;
+                    }
+                    //free up memory held by SharedPtr's count
+                    delete this->count;
+                }
+            }
+
 };
 
 #endif // SHARED_PTR_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,87 @@
 #include "SharedPtr.h"
+#include <iostream>
+#include <cassert>
 
-int main()
-{
-    
+class TestObject {
+public:
+    int value;
+    TestObject(int val) : value(val) {}
+};
+
+void testDefaultConstructor() {
+    SharedPtr<TestObject> sp;
+    assert(sp.get() == nullptr);
+    assert(sp.getCount() == 0);
+    std::cout << "testDefaultConstructor passed!" << std::endl;
+}
+
+void testConstructorWithPointer() {
+    SharedPtr<TestObject> sp(new TestObject(10));
+    assert(sp.get() != nullptr);
+    assert(sp.get()->value == 10);
+    assert(sp.getCount() == 1);
+    std::cout << "testConstructorWithPointer passed!" << std::endl;
+}
+
+void testCopyConstructor() {
+    SharedPtr<TestObject> sp1(new TestObject(20));
+    SharedPtr<TestObject> sp2(sp1);
+    assert(sp1.get() == sp2.get());
+    assert(sp1.getCount() == 2);
+    assert(sp2.getCount() == 2);
+    std::cout << "testCopyConstructor passed!" << std::endl;
+}
+
+void testCopyAssignmentOperator() {
+    SharedPtr<TestObject> sp1(new TestObject(30));
+    SharedPtr<TestObject> sp2;
+    sp2 = sp1;
+    assert(sp1.get() == sp2.get());
+    assert(sp1.getCount() == 2);
+    assert(sp2.getCount() == 2);
+    std::cout << "testCopyAssignmentOperator passed!" << std::endl;
+}
+
+void testMoveConstructor() {
+    SharedPtr<TestObject> sp1(new TestObject(40));
+    SharedPtr<TestObject> sp2(std::move(sp1));
+    assert(sp1.get() == nullptr);
+    assert(sp2.get() != nullptr);
+    assert(sp2.get()->value == 40);
+    assert(sp2.getCount() == 1);
+    std::cout << "testMoveConstructor passed!" << std::endl;
+}
+
+void testMoveAssignmentOperator() {
+    SharedPtr<TestObject> sp1(new TestObject(50));
+    SharedPtr<TestObject> sp2;
+    sp2 = std::move(sp1);
+    assert(sp1.get() == nullptr);
+    assert(sp2.get() != nullptr);
+    assert(sp2.get()->value == 50);
+    assert(sp2.getCount() == 1);
+    std::cout << "testMoveAssignmentOperator passed!" << std::endl;
+}
+
+void testDestructor() {
+    SharedPtr<TestObject> sp1(new TestObject(60));
+    {
+        SharedPtr<TestObject> sp2(sp1);
+        assert(sp1.getCount() == 2);
+    }
+    assert(sp1.getCount() == 1);
+    std::cout << "testDestructor passed!" << std::endl;
+}
+
+int main() {
+    testDefaultConstructor();
+    testConstructorWithPointer();
+    testCopyConstructor();
+    testCopyAssignmentOperator();
+    testMoveConstructor();
+    testMoveAssignmentOperator();
+    testDestructor();
+
+    std::cout << "All tests passed!" << std::endl;
+    return 0;
 }
